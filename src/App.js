708600be43970a2ba7557ace4757c86f569a4715
Ivan Scarciglia <ut82441@credemtel.cloud>
2019-12-17 11:15:48 +0100

    add navigation withRouter HOC in menu-items

--- src/App.js
+++ src/App.js
@@ -1,15 +1,24 @@
 import React from 'react';
-import {BrowserRouter} from "react-router-dom";
 
 import './App.css';
 import HomePage from "./pages/homepage/hompage.component";
+import Route from "react-router-dom/es/Route";
+import Switch from "react-router-dom/es/Switch";
 
+const HatsPage = () => (
+    <div>
+        <h1>HATS PAGE</h1>
+    </div>
+);
 
 function App() {
   return (
-    <BrowserRouter>
-        <HomePage/>
-    </BrowserRouter>
+    <div>
+        <Switch>
+            <Route exact path='/' component={HomePage} />
+            <Route path='/hats' component={HatsPage} />
+        </Switch>
+    </div>
   );
 }
 
