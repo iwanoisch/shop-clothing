a8e199f7a7517cf07ed9424b091754a6d9b6f938
Ivan Scarciglia <ut82441@credemtel.cloud>
2019-12-17 13:15:38 +0100

    adding Header link component

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import React from 'react';
 
 import './App.css';
 import HomePage from "./pages/homepage/hompage.component";
-import Route from "react-router-dom/es/Route";
-import Switch from "react-router-dom/es/Switch";
+import {Route, Switch} from "react-router-dom";
+
 import ShopPage from "./pages/shop/shop.component";
+import Header from "./components/header/header-component";
 
 function App() {
   return (
     <div>
+        <Header />
         <Switch>
             <Route exact path='/' component={HomePage} />
             <Route path='/shop' component={ShopPage} />
--- src/components/collection-item/collection-item.jsx
+++ src/components/collection-item/collection-item.jsx
@@ -4,12 +4,11 @@ import './collection-item.scss';
 
 const CollectionItem = ({id, name, price, imageUrl}) => (
     <div key={id} className="collection-item">
-        <div className="image" style={{backgroundImage: `url(${imageUrl})`}} >
+        <div className="image" style={{backgroundImage: `url(${imageUrl})`}} />
             <div className="collection-footer">
                 <span className="name">{name}</span>
                 <span className="price">{price}</span>
             </div>
-        </div>
     </div>
 );
 
