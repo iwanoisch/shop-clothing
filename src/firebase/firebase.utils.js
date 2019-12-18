faceb394510036f354f13101dd189bb0ed900d37
Ivan Scarciglia <ut82441@credemtel.cloud>
2019-12-18 16:24:36 +0100

    new login handling submit

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -1,26 +1,49 @@
-import firebase from 'firebase/app'
-import 'firebase/firestore'
-import 'firebase/auth'
+import firebase from 'firebase/app';
+import 'firebase/firestore';
+import 'firebase/auth';
 
-
-let firebaseConfig = {
-    apiKey: "AIzaSyALy16En6lXPuU4HhZHRraDYAmi_e6R7lc",
-    authDomain: "clothing-db-c4179.firebaseapp.com",
-    databaseURL: "https://clothing-db-c4179.firebaseio.com",
-    projectId: "clothing-db-c4179",
-    storageBucket: "clothing-db-c4179.appspot.com",
-    messagingSenderId: "641951906129",
-    appId: "1:641951906129:web:b014e41cee7775d926bb70",
-    measurementId: "G-2K0880VVYM"
+const config = {
+  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
+  authDomain: 'crwn-db.firebaseapp.com',
+  databaseURL: 'https://crwn-db.firebaseio.com',
+  projectId: 'crwn-db',
+  storageBucket: 'crwn-db.appspot.com',
+  messagingSenderId: '850995411664',
+  appId: '1:850995411664:web:7ddc01d597846f65'
 };
-// Initialize Firebase
-firebase.initializeApp(firebaseConfig);
+
+firebase.initializeApp(config);
+
+export const createUserProfileDocument = async (userAuth, additionalData) => {
+  if (!userAuth) return;
+
+  const userRef = firestore.doc(`users/${userAuth.uid}`);
+
+  const snapShot = await userRef.get();
+
+  if (!snapShot.exists) {
+    const { displayName, email } = userAuth;
+    const createdAt = new Date();
+    try {
+      await userRef.set({
+        displayName,
+        email,
+        createdAt,
+        ...additionalData
+      });
+    } catch (error) {
+      console.log('error creating user', error.message);
+    }
+  }
+
+  return userRef;
+};
 
 export const auth = firebase.auth();
 export const firestore = firebase.firestore();
 
 const provider = new firebase.auth.GoogleAuthProvider();
-provider.setCustomParameters({prompt: 'select_account'});
+provider.setCustomParameters({ prompt: 'select_account' });
 export const signInWithGoogle = () => auth.signInWithPopup(provider);
 
 export default firebase;
--- src/pages/shop/shop.component.jsx
+++ src/pages/shop/shop.component.jsx
@@ -1,27 +1,28 @@
 import React from 'react';
-import SHOP_DATA from "./shop.data";
-import CollectionPreview from "../../components/collection-preview-component/collection-preview-component";
 
-class ShopPage extends React.Component{
-    constructor(props) {
-        super(props);
+import SHOP_DATA from './shop.data.js';
 
-        this.state = {
-            collections: SHOP_DATA,
-        }
-    }
+import CollectionPreview from '../../components/collection-preview/collection-preview';
 
-    render() {
-        const {collections} = this.state;
-        return(
-            <div className='shop-page'>
-                {collections && collections.map( ({id, ...otherCollectionsProps}) => (
-                    <CollectionPreview key={id} {...otherCollectionsProps}/>
-                ))}
-            </div>
+class ShopPage extends React.Component {
+  constructor(props) {
+    super(props);
 
-        )
-    }
+    this.state = {
+      collections: SHOP_DATA
+    };
+  }
+
+  render() {
+    const { collections } = this.state;
+    return (
+      <div className='shop-page'>
+        {collections.map(({ id, ...otherCollectionProps }) => (
+          <CollectionPreview key={id} {...otherCollectionProps} />
+        ))}
+      </div>
+    );
+  }
 }
 
-export default ShopPage
+export default ShopPage;
