22789600e41bcf3aeaac41dde6166566e65ecf71
Ivan Scarciglia <ut82441@credemtel.cloud>
2019-12-30 17:35:03 +0100

    insert new commit

--- src/firebase/firebase.utils.js
+++ src/firebase/firebase.utils.js
@@ -3,13 +3,14 @@ import 'firebase/firestore';
 import 'firebase/auth';
 
 const config = {
-  apiKey: 'AIzaSyCdHT-AYHXjF7wOrfAchX4PIm3cSj5tn14',
-  authDomain: 'crwn-db.firebaseapp.com',
-  databaseURL: 'https://crwn-db.firebaseio.com',
-  projectId: 'crwn-db',
-  storageBucket: 'crwn-db.appspot.com',
-  messagingSenderId: '850995411664',
-  appId: '1:850995411664:web:7ddc01d597846f65'
+  apiKey: "AIzaSyALy16En6lXPuU4HhZHRraDYAmi_e6R7lc",
+  authDomain: "clothing-db-c4179.firebaseapp.com",
+  databaseURL: "https://clothing-db-c4179.firebaseio.com",
+  projectId: "clothing-db-c4179",
+  storageBucket: "clothing-db-c4179.appspot.com",
+  messagingSenderId: "641951906129",
+  appId: "1:641951906129:web:b014e41cee7775d926bb70",
+  measurementId: "G-2K0880VVYM"
 };
 
 firebase.initializeApp(config);
